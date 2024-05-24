45bad9263bd7fec2b8069020162b244d0aa76b88
noeFly <noeFly@proton.me>
2024-05-24 19:12:20 +0800

    .

--- way_control/main.py
+++ way_control/main.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+import pygsheets
 from paho.mqtt import client as mqtt
 
 from alert import main as line_notify
@@ -9,6 +10,9 @@ from toolbox import log, unix_timestamp
 client: any
 wayin_car: bool = False
 wayout_car: bool = False
+apikey: any
+sheet: any
+worksheet: any
 
 
 def on_connect(cli, __, ___, rc) -> None:
@@ -25,7 +29,7 @@ def on_connect(cli, __, ___, rc) -> None:
 
 
 def on_message(_, __, message) -> None:
-    global client, wayin_car, wayout_car
+    global client, wayin_car, wayout_car, worksheet
 
     if message.topic == 'noefly/mqtt/wayIn':
         if message.payload == b'module.sonar.enable':
@@ -41,20 +45,26 @@ def on_message(_, __, message) -> None:
         if not wayin_car:
             return
         if not check_database(message.payload):
-            client.publish('noefly/mqtt/wayIn', 'id.notfound')
+            worksheet.update_cell('B1', 'id.notfound')
+            worksheet.update_cell('C1', unix_timestamp())
+            # client.publish('noefly/mqtt/wayIn', 'id.notfound')
             return
-        client.publish('noefly/mqtt/wayIn', 'gate.action')
+        worksheet.update_cell('B1', 'gate.action')
+        worksheet.update_cell('C1', unix_timestamp())
+        # client.publish(topic='noefly/mqtt/wayIn', payload='gate.action', retain=True)
         add_parking(message.payload)
     elif message.topic == 'noefly/mqtt/rfid1':
         if not wayout_car:
             return
         if not check_database(message.payload):
-            client.publish('noefly/mqtt/wayOut', 'id.notfound')
+            worksheet.update_cell('B2', 'gate.action')
+            worksheet.update_cell('C2', unix_timestamp())
+            # client.publish(topic='noefly/mqtt/wayOut', payload='id.notfound', retain=True)
             return
-        client.publish('noefly/mqtt/wayOut', 'gate.action')
+        # client.publish('noefly/mqtt/wayOut', 'gate.action')
         del_parking(message.payload)
     elif message.topic == 'noefly/mqtt/water':
-        con = sqlite3.connect('./../backend.db')
+        con = sqlite3.connect('./../ backend.db')
         cur = con.cursor()
         cur.execute('INSERT INTO water VALUES ( ?, ? )', (unix_timestamp(), message.payload))
         con.commit()
@@ -65,15 +75,27 @@ def on_message(_, __, message) -> None:
 
 
 def main() -> None:
-    global client
+    global client, apikey, sheet, worksheet
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message  # 註冊 on_message 回調函數
     client.username_pw_set("", "")
     client.connect('test.mosquitto.org', 1883)
     client.loop_start()  # 啟動非阻塞迴圈
+    apikey = pygsheets.authorize(service_file='./apikey.json')
+    sheet = apikey.open_by_key('1jo6k-zgXLeXxPyXKXRFVVxdgCi_HrYuFBM489vhSes8')
+    worksheet = sheet.worksheet_by_title('工作表1')
     while True:
         pass  # 保持主程序運行
+
+
+def last_row(ws) -> int:
+    i: int = 0
+    while True:
+        i += 1
+        cell = ws.cell(f'A{i}').value
+        if cell == '':
+            return i - 1
 
 
 if __name__ == '__main__':
