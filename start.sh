ed018dada80959396673ebe623c5aaeee19af992
noeFly <noefly@proton.me>
2024-05-27 01:48:40 +0800

    rewrite startup script

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
-cd ./opendata
-python3 ./main.py &
-cd ../sam
-python3 ./main.py &
-cd ../way_control
-python3 ./main.py &
+python3 ./way_control/main.py &
+python3 ./opendata/main.py &
+python3 ./sam/main.py &
